cf3eaaa572b28ef653b3243444fe22c148513a2b
Likitha Shetty <likitha.shetty@citrix.com>
2014-10-31 16:26:51 +0530

    BUG-ID: CS-20766. Revert "CLOUDSTACK-7150. [VMware] Global config 'vm.instancename' is not honored."
    This reverts commit b3e4c6d6dc9bf57aa880ab100a76027b2a12053d.
    Reviewed-By: Devdeep Singh

--- plugins/hypervisors/vmware/src/com/cloud/hypervisor/vmware/resource/VmwareResource.java
+++ plugins/hypervisors/vmware/src/com/cloud/hypervisor/vmware/resource/VmwareResource.java
@@ -1970,8 +1970,7 @@ public class VmwareResource implements StoragePoolResource, ServerResource, Vmwa
             vmInternalCSName = vmSpec.getName();
             if (_instanceNameFlag == true) {
                 String[] tokens = vmInternalCSName.split("-");
-                assert (tokens.length >= 3); // vmInternalCSName has format i-x-y-<instance.name>
-                vmNameOnVcenter = String.format("%s-%s-%s-%s", tokens[0], tokens[1], tokens[2], vmSpec.getHostName());
+                vmNameOnVcenter = String.format("%s-%s-%s", tokens[0], tokens[1], vmSpec.getHostName());
             }
             else
                 vmNameOnVcenter = vmSpec.getName();
