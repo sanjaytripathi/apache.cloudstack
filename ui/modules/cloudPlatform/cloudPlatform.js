d7367779add004786c5cbd3d76fcfd54b53b71bc
Brian Federle <brian.federle@citrix.com>
2014-11-12 09:55:27 -0800

    CLOUDSTACK-7645: UI: Fix method for extending dictionary
    Instead of mapping both dictionary JSP files to separate objects, extend
    dictionary2's object onto single 'dictionary' object.
    
    -- The previous approach was causing issues on certain dialogs, which were not
    opening due to possible missing labels.
    
    BUG-ID: CS-22955
    REVIEWED-BY: Jessica

--- ui/modules/cloudPlatform/cloudPlatform.js
+++ ui/modules/cloudPlatform/cloudPlatform.js
@@ -79,9 +79,7 @@
 
             // Replace 'CloudStack' -> 'CloudPlatform'
             cloudStack.localizationFn = function(str) {
-                // look in dictionary first; if not found, try dictionary2
-                var localized = dictionary[str] ? dictionary[str] : dictionary2[str];
-                return localized ? replace(localized) : str; 
+                return dictionary[str] ? replace(dictionary[str]) : str;
             };
 
             // Remove unsupported languages
