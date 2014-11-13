55c83c4f5ecc73576ec72caac1b1b8a9826d42ce
Likitha Shetty <likitha.shetty@citrix.com>
2014-11-13 13:55:05 +0530

    BUG-ID: CS-26115, CS-27139, CS-27087. Post "Live VM/Storage migration", further operations on VM/Volume result in disk path inconsistencies between CCP DB and vCenter. Consolidate VM disks once VM/volumes are migrated. The above mentioned issues are in case of a linked clone VM where VM's disks are not consolidated, Reviewed-By: Devdeep Singh

--- vmware-base/src/com/cloud/hypervisor/vmware/mo/VirtualMachineMO.java
+++ vmware-base/src/com/cloud/hypervisor/vmware/mo/VirtualMachineMO.java
@@ -3127,4 +3127,17 @@ public class VirtualMachineMO extends BaseMO {
         }
         return count;
     }
+
+    public boolean consolidateVmDisks() throws Exception {
+        ManagedObjectReference morTask = _context.getService().consolidateVMDisksTask(_mor);
+        boolean result = _context.getVimClient().waitForTask(morTask);
+        if (result) {
+            _context.waitForTaskProgressDone(morTask);
+            return true;
+        } else {
+            s_logger.error("VMware ConsolidateVMDisks_Task failed due to " + TaskMO.getTaskFailureInfo(_context, morTask));
+        }
+        return false;
+    }
+
 }
