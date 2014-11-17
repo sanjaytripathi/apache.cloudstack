4266e08495f0c82bda27069ca6da15d575c5eeb1
Jessica Wang <jessicawang@apache.org>
2014-11-17 12:57:53 -0800

    BUG-ID: CS-25933: UI > Infrastructure > Primary Storage > detailView > add "View Volumes" link that will list all volumes under this primary storage when being clicked. Reviewed-by: Brian.

--- ui/scripts/system.js
+++ ui/scripts/system.js
@@ -17415,6 +17415,10 @@
                     
                     detailView: {
                         name: "Primary storage details",
+                        viewAll: {
+                            label: 'label.volumes',
+                            path: 'storage.volumes'
+                        },
                         isMaximized: true,
                         actions: {
                             edit: {
