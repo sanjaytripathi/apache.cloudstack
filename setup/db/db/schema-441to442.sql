89ff8834902062955f400789524e3ab58034db3f
Saksham Srivastava <saksham.srivastava@citrix.com>
2014-12-01 11:55:18 +0530

    BUG-ID: CS-30120: Add new 4.4.2 db upgarde path in CCP to keep in sync with ACS Reviewed-by: Devdeep
    Added new 4.4.2 path in CCP similar to ACS

--- setup/db/db/schema-441to442.sql
+++ setup/db/db/schema-441to442.sql
@@ -15,19 +15,7 @@
 -- specific language governing permissions and limitations
 -- under the License.
 
---
--- Schema cleanup from 4.4.0 to 4.5.0
---
+--;
+-- Schema upgrade from 4.4.1 to 4.4.2;
+--;
 
-UPDATE `cloud`.`configuration`
-SET name = 'router.template.xenserver'
-Where name = 'router.template.xen';
-
-UPDATE `cloud`.`configuration`
-SET name = 'xenserver.nics.max',
-description = 'Maximum allowed nics for Vms created on XenServer'
-Where name = 'xen.nics.max';
-
-UPDATE `cloud`.`configuration`
-SET value = 'XenServer'
-Where value = 'Xen';
